f54d3a039005d24a9030a3da80e85438d13d2059
⚡️ Bilali ⚡️ <billo26085@gmail.com>
2025-09-06 22:07:10 +0500

    Add files via upload

---
+++ cularor.js
@@ -0,0 +1,26 @@
+let input = document.querySelector('#inputs');
+let buttons = document.querySelectorAll('button');
+let buttonsArray = Array.from(buttons);
+let string = '';
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if(e.target.innerHTML == 'DEL')  {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC') {
+            string = '';
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+            
+        console.log (e.target.innerHTML);
+    });
+});
